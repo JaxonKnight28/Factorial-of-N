8f9c5e01d5936274e9e1a968ded924df4a84ebd8
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2021-09-24 08:22:17 -0600

    made it closer

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
+let total = 1;
+let result = 0
 function displayResult() {
-  let result = 0
+  result = 0
   let inputNum = document.getElementById('number').value;
-  console.log(factorial(inputNum))
   result = factorial(inputNum);
   document.getElementById('output').innerHTML = result;
 }
 
 
-let total = 1;
+
 function factorial(n) {
   if (n < 1) {
     return total;
@@ -18,4 +19,3 @@ function factorial(n) {
   }
 }
 
-//console.log(factorial(7));
