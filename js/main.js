18553b4c6526e870c9c3d19147edaf42492df48a
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2021-09-27 07:33:39 -0600

    cleaned up the file a little bit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 let total = 1;
 let result = 0;
 function displayResult() {
-  
   let inputNum = document.getElementById('number').value;
   total = 1;
   result = factorial(inputNum);
   document.getElementById('output').innerHTML = result;
 }
-
-
 
 function factorial(n) {
   if (n < 1) {
